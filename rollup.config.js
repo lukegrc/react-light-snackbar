b41bc4736778c1d6ce4222e51cafabfd6c69ed0d
Luke Grech <lukegrech2002@gmail.com>
2025-08-26 22:12:04 +0200

    chore: bump version and improve rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,23 @@
-const resolve = require('@rollup/plugin-node-resolve');
-const commonjs = require('@rollup/plugin-commonjs');
-const typescript = require('@rollup/plugin-typescript');
-const peerDepsExternal = require('rollup-plugin-peer-deps-external');
-const { dts } = require('rollup-plugin-dts');
+const resolve = require("@rollup/plugin-node-resolve");
+const commonjs = require("@rollup/plugin-commonjs");
+const typescript = require("@rollup/plugin-typescript");
+const peerDepsExternal = require("rollup-plugin-peer-deps-external");
+const { dts } = require("rollup-plugin-dts");
 
-const packageJson = require('./package.json');
+const packageJson = require("./package.json");
 
 module.exports = [
   {
-    input: 'src/index.ts',
+    input: "src/index.ts",
     output: [
       {
         file: packageJson.main,
-        format: 'cjs',
+        format: "cjs",
         sourcemap: true,
       },
       {
         file: packageJson.module,
-        format: 'esm',
+        format: "esm",
         sourcemap: true,
       },
     ],
@@ -25,13 +25,13 @@ module.exports = [
       peerDepsExternal(),
       resolve(),
       commonjs(),
-      typescript({ tsconfig: './tsconfig.json' }),
+      typescript({ tsconfig: "./tsconfig.json" }),
     ],
-    external: ['react', 'react-dom'],
+    external: ["react", "react-dom", "react/jsx-runtime"],
   },
   {
-    input: 'dist/index.d.ts',
-    output: [{ file: 'dist/index.d.ts', format: 'es' }],
+    input: "dist/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "es" }],
     plugins: [dts()],
   },
 ];
